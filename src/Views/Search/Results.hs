22c7e9916abf73ce4fe297c3b2c6b81ec640a1ef
adnihilum <alex.adnihilum@gmail.com>
2019-01-08 17:30:41 +0500

    add a search form

--- src/Views/Search/Results.hs
+++ src/Views/Search/Results.hs
@@ -4,8 +4,9 @@ import Prelude hiding (div, head, id)
 
 import Data.Maybe (fromMaybe)
 import Data.Text (Text, append)
-import Text.Blaze.Html5 ((!), a, div, h1, p, text)
-import Text.Blaze.Html5.Attributes (class_, href, id)
+import Kinopoisk.SearchUrl
+import Text.Blaze.Html5 (Html, (!), a, button, div, form, h1, input, label, option, p, select, text, textValue)
+import Text.Blaze.Html5.Attributes (action, class_, for, href, id, method, multiple, name, type_, value)
 import qualified Views.Layout
 import Views.Utils (blaze)
 import Web.Scotty (ActionM)
@@ -16,12 +17,26 @@ view request movieTitle =
   Views.Layout.layout "Search" $ do
     div ! class_ "container" $ do
       div ! class_ "jumbotron" $ do
-        h1 "Scotty Starter"
-        p $
-          text $
-          "Welcome to the Scotty Starter template, equipped with Twitter Bootstrap 3.0 and HTML5 boilerplate " `append`
-          fromMaybe "" request
-        p $ do
-          a ! class_ "btn btn-lg btn-primary" ! id "fb" ! href "#navbar" $ "Facebook"
-          a ! class_ "btn btn-lg btn-danger" ! id "gmail" ! href "#navbar" $ "Gmail"
+        h1 "Случайная выборка"
         p $ text $ "First found movie: " `append` fromMaybe "Not Found" movieTitle
+        searchForm
+
+-- from_year to_year genre
+searchForm :: Html
+searchForm = do
+  form ! action "/" ! method "get" $ do
+    div ! class_ "form-group" $ do
+      simpleInput "from_year" "from_year" "From year:"
+      simpleInput "to_year" "to_year" "To year:"
+      multiSelectList "type" "type" "Content type:" $
+        map (\(ContentType code title) -> (textValue code, text title)) contentTypeList
+      submitButton "Search"
+  where
+    simpleInput inputName inputId inputLabel = do
+      label ! for inputId $ inputLabel
+      input ! name inputName ! type_ "text" ! class_ "form-control" ! id inputId
+    multiSelectList inputName inputId inputLabel values = do
+      label ! for inputId $ inputLabel
+      select ! name inputName ! class_ "form-control" ! id inputId $
+        mapM_ (\(optId, optLabel) -> option ! value optId $ optLabel) values
+    submitButton buttonLabel = input ! type_ "submit" ! class_ "btn btn-default" ! value buttonLabel
